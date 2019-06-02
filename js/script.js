5e6ccc43301e0c893b4c5330f04215e092ce643b
wiatr <krzywiatr@gmail.com>
2019-06-02 17:43:46 +0200

    X

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+
+$(function () {
+
+	$('.arrow').click(function() {
+		$.scrollTo(document.getElementById('skills'), 800);
+	});
+
+});
+
+
+
+new TypeIt('#typedtext', {
+	strings: ["Pomagamy odkrywać", "własne możliwości"],
+	speed: 50,
+	waitUntilVisible: true
+  }).go();
+
+  $(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $("header").addClass("active");
+    } else {
+       $("header").removeClass("active");
+    }
+});
